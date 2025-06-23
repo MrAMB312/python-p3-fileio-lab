c292bf366ea870e2774a568daf292a64c71c8368
Alex Blomarz <a.blomarz@gmail.com>
2025-06-22 20:07:14 -0500

    complete python-p3-fileio-lab third commit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,15 +1,11 @@
 def write_file(file_name, file_content):
     with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file:
-        file.write(file_content)
+        return file.write(file_content)
 
 def append_file(file_name, append_content):
     with open(f'{file_name}.txt', mode='a', encoding='utf-8') as file:
-        file.write(append_content)
+        return file.write(append_content)
 
 def read_file(file_name):
-    text_file = open(f'{file_name}.txt', encoding='utf-8')
-    print(text_file.read())
-
-write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
-append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
-read_file(file_name="logfile")
+    with open(f'{file_name}.txt', encoding='utf-8') as file:
+        return file.read()
